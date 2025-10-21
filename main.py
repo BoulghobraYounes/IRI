4cfade3ff9defedfc05362b15a22891952ceb0a8
BoulghobraYounes <younes.boulghobrapro@gmail.com>
2025-10-21 21:52:33 +0100

    completed dictionnary + changed all images to jpg + fixed main errors

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from index import dictionnaire
 import cv2 as cv
+import os 
 
-def show_image(image):
-    cv.imshow('Image', image)
+def show_image(image, max_width=800, max_height=600):
+    h, w = image.shape[:2]
+
+    #(keep aspect ratio)
+    scale = min(max_width / w, max_height / h, 2.0)  # don't enlarge small images
+
+    new_w, new_h = int(w * scale), int(h * scale)
+    resized = cv.resize(image, (new_w, new_h), interpolation=cv.INTER_AREA)
+    cv.imshow('Image', resized)
     cv.waitKey(0)
     cv.destroyAllWindows()
 
 if __name__ == '__main__':
-    print("Entrer un mot pour rechercher: ")
-    recherche = input()
+    recherche = input("Entrer un mot pour rechercher: ").lower()
     result_images = []
 
-    path = 'images/'
+    bas_path = 'images/'
     ou = False
     mot_cle = recherche.split(' ')
     
@@ -37,7 +44,7 @@ if __name__ == '__main__':
     else: 
         print(f'{len(result_images)} Images Trouvées')
         for image in result_images:
-            path += image
-            print(path)
-            image = cv.imread(path)
+            full_path = os.path.join(bas_path, image)
+            print(full_path)
+            image = cv.imread(full_path)
             show_image(image)
